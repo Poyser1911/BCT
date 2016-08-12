34259fe1759ae6911031e63055930a89a504adae
Poyser <damanipoyser1911@yahoo.com>
2016-08-12 10:00:45 -1200

    first commit

---
+++ BCT/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Threading;
+using Q3query;
+using Shell32;
+using IWshRuntimeLibrary;
+
+namespace BCT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Globals 
+        static string ip = "38.130.203.243";
+        static int port = 28930;
+        static query d = new query(ip, port);
+        Dictionary<string, string> dvars = d.GetInfoDvars();
+        Thread refresh;
+        #endregion 
+
+        #region Main
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           refresh = new Thread(AutoRefresh);
+           refresh.Start();
+        }
+        #endregion
+
+        #region Auto Refresh
+        public void AutoRefresh()
+        {
+            bool autore = false;
+            while (true)
+            {
+                Dispatcher.Invoke(new Action(() =>
+{
+    autore = (bool)autorefresh.IsChecked;
+}));
+                while (autore)
+                {
+                    DataTable players = d.GetplayersTable();
+                    Dispatcher.Invoke(new Action(() =>
+    {
+    if (online == null)
+        return;
+    Dictionary<string, string> dvars = d.GetInfoDvars();
+    playersbox.Items.Clear();
+    playersbox2.Items.Clear();
+    playersbox3.Items.Clear();
+    if (players == null || dvars == null)
+    {
+        online.Content = "Sever Offline - Check Internet";
+        return;
+    }
+    online.Content = "Players Online: " + players.Rows.Count + "/" + dvars["sv_maxclients"] + "  Gametype: " + dvars["g_gametype"] + "  Map: " + dvars["mapname"].Substring(2).Substring(0, 2).ToUpper() + dvars["mapname"].Substring(4);
+    foreach (DataRow dr in players.Rows)
+    {
+        RichTextBox x = new RichTextBox();
+        x.Background = Brushes.Transparent;
+        x.BorderBrush = Brushes.Transparent;
+        x.BorderThickness = new Thickness(0, 0, 0, 0);
+        x.Width = playersbox.Width;
+        x.IsReadOnly = true;
+        d.Q3Out(x, dr["name"].ToString());
+        playersbox.Items.Add(x);
+        playersbox2.Items.Add(dr["score"]);
+        playersbox3.Items.Add(dr["ping"]);
+    }
+    autore = (bool)autorefresh.IsChecked;
+}));
+                    Thread.Sleep(1000);
+                }
+                Thread.Sleep(1000);
+            }
+
+        }
+        #endregion
+
+        #region Kill Processs 
+        private void Window_Closed_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = System.Diagnostics.Process.GetCurrentProcess();
+            p.Kill();
+        }
+        #endregion
+
+        #region Join Server
+        public void joinserver(string ip, string port)
+        {
+            if (Properties.Settings.Default.gamepath == string.Empty)
+            {
+                System.Windows.Forms.DialogResult result = System.Windows.Forms.MessageBox.Show("Do you want to set game path?", "Game path not found", System.Windows.Forms.MessageBoxButtons.YesNo, System.Windows.Forms.MessageBoxIcon.Question);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                    SetupPath();
+                return;
+            }
+
+            try
+            {
+                string inkpath = AppDomain.CurrentDomain.BaseDirectory + "\\cod4game.lnk";
+                var wsh = new IWshShell_Class();
+                IWshRuntimeLibrary.IWshShortcut shortcut = wsh.CreateShortcut(inkpath) as IWshRuntimeLibrary.IWshShortcut;
+                shortcut.Arguments = "connect " + ip + ":" + port;
+                shortcut.TargetPath = Properties.Settings.Default.gamepath;
+                shortcut.WorkingDirectory = System.IO.Path.GetDirectoryName(Properties.Settings.Default.gamepath);
+                shortcut.Save();
+
+                System.Diagnostics.Process.Start(AppDomain.CurrentDomain.BaseDirectory + "\\cod4game.lnk");
+                System.IO.File.Delete(AppDomain.CurrentDomain.BaseDirectory + "\\cod4game.lnk");
+            }
+            catch (Exception c)
+            {
+                if (c.Message.Contains("Access is denied"))
+                    MessageBox.Show("Try moving files to Desktop/Documents", "Unable to Launch Game", MessageBoxButton.OK, MessageBoxImage.Error);
+                else
+                    MessageBox.Show(c.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void SetupPath()
+        {
+            System.Windows.Forms.OpenFileDialog file = new System.Windows.Forms.OpenFileDialog();
+            file.Filter = "Cod4|iw3mp.exe|Renamed it(*.exe)|*.exe|All Files(*.*)|*.*";
+            if (System.IO.Directory.Exists(@"C:\Program Files (x86)\Activision\Call of Duty 4 - Modern Warfare"))
+                file.InitialDirectory = @"C:\Program Files (x86)\Activision\Call of Duty 4 - Modern Warfare";
+            else if (System.IO.Directory.Exists(@"C:\Program Files (x86)\Steam\SteamApps\common\Call of Duty 4"))
+                file.InitialDirectory = @"C:\Program Files (x86)\Steam\SteamApps\common\Call of Duty 4";
+
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.Windows.Forms.DialogResult result = System.Windows.Forms.MessageBox.Show("Set Game Path: " + file.FileName, "Save Settings", System.Windows.Forms.MessageBoxButtons.OKCancel, System.Windows.Forms.MessageBoxIcon.Question);
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    Properties.Settings.Default.gamepath = file.FileName;
+                    Properties.Settings.Default.Save();
+                    MessageBox.Show("Saved: " + Properties.Settings.Default.gamepath, "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+            }
+        }
+
+        private void join_Click(object sender, RoutedEventArgs e)
+        {
+            joinserver(ip, port.ToString());
+        }
+
+        private void logo_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            joinserver(ip, port.ToString());
+        }
+        #endregion
+
+    }
+}
---
+++ BCT/Q3class.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Data;
+using System.Windows.Documents;
+using System.Windows;
+using System.Windows.Media;
+
+
+/* q3query.dll - Quake 3 query class
+ *
+ * Copyright (C) 2015 Damani Poyser
+ * Author(s): Damani Poyser (damanipoyser1911@yahoo.com)
+ *
+ */
+
+namespace Q3query
+{
+    public class query
+    {
+        string rconpass = null;
+
+        Socket sck;
+        IPEndPoint server;
+
+        public query(string ip, int port)
+        {
+            //IPHostEntry Host = Dns.GetHostEntry(ip);
+            //MessageBox.Show(Host.AddressList[0].ToString());
+            server = new IPEndPoint(IPAddress.Parse(ip), port);
+        }
+
+        public void SetRconPassword(string pass)
+        {
+            rconpass = pass;
+        }
+
+        public byte[] Tobyte(string msg)
+        {
+            byte[] bufferTemp = Encoding.ASCII.GetBytes(msg);
+            byte[] bufferSend = new byte[bufferTemp.Length + 4];
+
+
+            for (int i = 0; i < 4; i++)
+                bufferSend[i] = 0xFF;
+            for (int i = 0; i < bufferTemp.Length; i++)
+                bufferSend[i + 4] = bufferTemp[i];
+
+            return bufferSend;
+
+        }
+        public string Send(string msg)
+        {
+            try
+            {
+                sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                sck.Connect(server);
+
+                byte[] buffer = new byte[2000];
+                sck.Send(Tobyte(msg));
+
+                sck.ReceiveTimeout = 600;
+                sck.Receive(buffer);
+                return Encoding.Default.GetString(buffer).Replace('\0', ' ').Trim();
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
+        }
+
+        public Dictionary<string, string> GetInfoDvars()
+        {
+            string status = Send("getstatus");
+            if (status == null)
+                return null;
+            string[] dvarlist = status.Split('\n')[1].Split('\\');
+            var dvars = new Dictionary<string, string>();
+
+            for (int i = 1; i < dvarlist.Length; i += 2)
+                dvars[dvarlist[i]] = dvarlist[i + 1];
+
+            return dvars;
+        }
+
+        public DataTable GetplayersTable()
+        {
+            string status = Send("getstatus");
+            if (status == null)
+                return null;
+
+            string[] playerlist = status.Split('\n');
+            DataTable playerstable = new DataTable();
+            playerstable.Columns.Add(new DataColumn("name"));
+            playerstable.Columns.Add(new DataColumn("score"));
+            playerstable.Columns.Add(new DataColumn("ping"));
+
+            for (int i = 2; i < playerlist.Length; i++)
+                playerstable.Rows.Add(playerlist[i].Split('"')[1], playerlist[i].Split(' ')[0], playerlist[i].Split(' ')[1]);
+            return playerstable;
+        }
+
+        public string Rconcmd(string cmd)
+        {
+            if (rconpass == null)
+                return "Rconpassword Not Set use function SetRconPassword(yourpass) first";
+            return Send("rcon " + rconpass + " " + cmd).Substring(9).Trim();
+        }
+
+        public void Q3Out(System.Windows.Controls.RichTextBox help, string text)
+        {
+            var colour = Brushes.White;
+            help.Document.Blocks.Clear();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '^')
+                {
+                    switch (text[i + 1])
+                    {
+                        case '1': colour = Brushes.Red;
+                            break;
+                        case '2': colour = Brushes.Green;
+                            break;
+                        case '3': colour = Brushes.Yellow;
+                            break;
+                        case '4': colour = Brushes.Blue;
+                            break;
+                        case '5': colour = Brushes.Aqua;
+                            break;
+                        case '6': colour = Brushes.Purple;
+                            break;
+                        case '7': colour = Brushes.White;
+                            break;
+                        case '8': colour = Brushes.Gray;
+                            break;
+                        case '9': colour = Brushes.Brown;
+                            break;
+                        case '0': colour = Brushes.Black;
+                            break;
+                        default:
+                            break;
+                    }
+                    i = i + 2;
+                    TextRange rangeOfWord = new TextRange(help.Document.ContentEnd, help.Document.ContentEnd);
+                    rangeOfWord.Text = text[i].ToString();
+                    rangeOfWord.ApplyPropertyValue(TextElement.ForegroundProperty, colour);
+                    rangeOfWord.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Regular);
+                }
+                else
+                {
+                    TextRange rangeOfWord = new TextRange(help.Document.ContentEnd, help.Document.ContentEnd);
+                    rangeOfWord.Text = text[i].ToString();
+                    rangeOfWord.ApplyPropertyValue(TextElement.ForegroundProperty, colour);
+                    rangeOfWord.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Regular);
+                }
+            }
+        }
+    }
+}
